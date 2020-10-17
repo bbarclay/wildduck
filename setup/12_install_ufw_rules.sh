15c3d64e829b800673a6a70b60a2a8075d62a9ea
bbarclay <barclaybrandon@hotmail.com>
2020-10-17 07:33:15 -0600

    Update 12_install_ufw_rules.sh
    I added a comment because we just spend 2 hours after running several of these scripts. We were trying to solve why we were locked out of our server, which happened to be a different port for ssh.

--- setup/12_install_ufw_rules.sh
+++ setup/12_install_ufw_rules.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+# Careful when you run this file. If you have it ssh installed on a different port, this script, or one in here like it, caused us to get locked out and then having
+# To try and figure out why we were locked out of our server took a while. So just be careful if you run this that you are using ssh on port 22. 
 
 OURNAME=12_install_ufw_rules.sh
 
